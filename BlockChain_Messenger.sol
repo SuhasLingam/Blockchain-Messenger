8e8f061514b5c708d71a7cbcbe34797eadcad1d7
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2022-12-24 08:23:20 +0530

    First Commit :white_check_mark:

--- BlockChain_Messenger.sol
+++ BlockChain_Messenger.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: UNIDENTIFIED
 
+pragma solidity >0.8.14;
+
+contract Messenger {
+
+
+    string public Mystring;
+
+    function Addstring (string memory _newString) public {
+        Mystring = _newString;
+    }
+
+
+
+
+
+}
